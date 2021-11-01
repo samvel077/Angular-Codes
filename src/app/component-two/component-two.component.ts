58485f4b5fa6789ead7a8203c03dc9913d801a19
Samvel <samvelhakobyan215@gmail.com>
2021-11-02 00:09:02 +0400

    Add paths configuration for styles in angular.json and for services in tsconfig.json, tsconfig.app.json

--- src/app/component-two/component-two.component.ts
+++ src/app/component-two/component-two.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ConsoleService } from "@services/console.service";
 
 @Component({
-   selector: 'app-component-two',
-   templateUrl: './component-two.component.html',
-   styleUrls: ['./component-two.component.scss']
+  selector: "app-component-two",
+  templateUrl: "./component-two.component.html",
+  styleUrls: ["./component-two.component.scss"],
 })
 export class ComponentTwoComponent implements OnInit {
+  data: any;
 
-   data: any
+  constructor(
+    private activatedroute: ActivatedRoute,
+    private router: Router,
+    private consoleSvc: ConsoleService
+  ) {
+    console.log(this.router.getCurrentNavigation().extras.state);
+  }
 
-   constructor(private activatedroute: ActivatedRoute, private router: Router,) {
-      console.log(this.router.getCurrentNavigation().extras.state)
-   }
+  ngOnInit() {
+    this.activatedroute.data.subscribe((data) => {
+      this.data = data;
+    });
 
-   ngOnInit() {
-      this.activatedroute.data.subscribe(data => {
-         this.data = data;
-      })
-
-      console.log(this.data)
-   }
-
+    console.log(this.data);
+  }
 }
